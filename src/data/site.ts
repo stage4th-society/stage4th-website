ce17a23a3740ff0a2f75f9822907db8cb24450fa
Nicole <suiwei.xie@gmail.com>
2026-08-06 14:33:35 +0800

    update upcoming event

--- src/data/site.ts
+++ src/data/site.ts
@@ -9,8 +9,8 @@ export const site = {
   xiaohongshu: "http://xhslink.com/m/7VOFBgRY4ym",
   wechatId: "van_fwd",
   logo: "/assets/logo/logo.png",
-  upcoming: "/assets/events/upcoming.jpg",
-  soding_link: "https://www.zeffy.com/en-CA/ticketing/staged-reading-series",
+  upcoming: "/assets/events/upcoming.png",
+  soding_link: "https://www.zeffy.com/en-CA/ticketing/staged-reading-series-the-weight-of-small-things-vol--2",
   past: "/assets/events/proof/poster.png",
   xhsQr: "/assets/social/xhs-qr.png",
   brand: {
